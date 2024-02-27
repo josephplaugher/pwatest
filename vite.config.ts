2bde788ecde84c5f20983d00098d9376431b4f43
josephplaugher <37582996+josephplaugher@users.noreply.github.com>
2024-02-27 13:27:18 -0800

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['favicon.ico', 'logo192x192.png', 'logo192x192.png'],
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'logo192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+  test: {
+    environment: "jsdom",
+  },
+});
